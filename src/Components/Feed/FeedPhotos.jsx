169251ff6ee44693192307df9550cfe26de7b85a
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2025-07-21 17:00:54 -0300

    feed Modal 1

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -4,9 +4,9 @@ import useFetch from "../../Hooks/useFetch";
 import { PHOTOS_GET } from "../../api";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
-import styles from "./FeedPhotos.module.css"
+import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
   React.useEffect(() => {
     async function fetchPhotos() {
@@ -25,8 +25,13 @@ const FeedPhotos = () => {
     return (
       <div>
         <ul className={`${styles.feed} animeLeft`}>
-          {data.map((photo) => <FeedPhotoItem key={photo.id} photo={photo}/>) }
-          
+          {data.map((photo) => (
+            <FeedPhotoItem
+              key={photo.id}
+              photo={photo}
+              setModalPhoto={setModalPhoto}
+            />
+          ))}
         </ul>
       </div>
     );
