d82f1fe31c5a807d40b48963a6cc916e673ee957
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2025-07-24 16:32:00 -0300

    feed modal 2

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import styles from "./PhotoContent.module.css";
 import { Link } from "react-router-dom";
 import PhotoComments from "./PhotoComments";
+import styles from "./PhotoContent.module.css";
 
-const PhotoContent = ({data}) => {
-  const {photo, comments} = data;
+const PhotoContent = ({ data }) => {
+  const { photo, comments } = data;
 
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.titulo} />
-        <div className={styles.details}>
-          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-          <span className={styles.vizualizacoes}>{photo.acessos}</span>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
           <h1 className="title">
-            <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
           <ul className={styles.attributes}>
             <li>{photo.peso} kg</li>
-            <li>{photo.idade} {photo.idade <= 1? 'ano' : 'anos'} </li>
-            <li>{photo.peso}</li>
-
+            <li>{photo.idade} anos</li>
           </ul>
         </div>
       </div>
-      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
